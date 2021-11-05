763efa508ec1c5ea462ff040025c5559411b93ea
AmrMossad93 <eng.amrmossad93@hotmail.com>
2021-11-05 16:10:12 +0200

    Structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
   {
@@ -7,6 +7,20 @@ const routes: Routes = [
     loadChildren: () =>
       import('./Pages/Home-Page/home-page.module').then(
         m => m.HomePageModule
+      )
+  },
+  {
+    path: 'about-me',
+    loadChildren: () =>
+      import('./Pages/About-Me-Page/about-me-page.module').then(
+        m => m.AboutMePageModule
+      )
+  },
+  {
+    path: 'resume',
+    loadChildren: () =>
+      import('./Pages/Resume-Page/resume-page.module').then(
+        m => m.ResumePageModule
       )
   },
   {
@@ -21,4 +35,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AmrMossadNewCV';
+  connectUrls = [
+    {url: 'home', page: 'Home Page'},
+    {url: 'about-me', page: 'About Me'},
+    {url: 'resume', page: 'My Resume'},
+  ]
 }
