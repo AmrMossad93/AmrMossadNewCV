f9052abc7e109f1dc8c054ecb40e5c9ffb89a78b
AmrMossad93 <eng.amrmossad93@hotmail.com>
2021-11-05 15:42:34 +0200

    Structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./Pages/Home-Page/home-page.module').then(
+        m => m.HomePageModule
+      )
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {path: '**', redirectTo: '/home'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
