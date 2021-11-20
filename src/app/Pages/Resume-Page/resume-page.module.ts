266696658a2c343712638e7ca13791d048d6bd16
AmrMossad93 <eng.amrmossad93@hotmail.com>
2021-11-20 15:25:39 +0200

    Resume

--- src/app/Pages/Resume-Page/resume-page.module.ts
+++ src/app/Pages/Resume-Page/resume-page.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ResumePageRoutingModule } from './resume-page-routing.module';
 import { ResumePageComponent } from './resume-page/resume-page.component';
 import {PrimeNgModule} from "../../DesignModules/prime-ng.module";
+import {AngularMaterialModule} from "../../DesignModules/angular-material.module";
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import {PrimeNgModule} from "../../DesignModules/prime-ng.module";
     imports: [
         CommonModule,
         ResumePageRoutingModule,
-        PrimeNgModule
+        PrimeNgModule,
+        AngularMaterialModule
     ]
 })
 export class ResumePageModule { }
