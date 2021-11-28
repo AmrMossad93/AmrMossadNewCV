685fa36cbcdd777f45c0058603cefa1943d96626
AmrMossad93 <50120275+AmrMossad93@users.noreply.github.com>
2021-11-28 04:32:38 +0200

    Filter Web Applications

--- src/app/Pages/Projects-Page/web-app-projects/web-app-projects.component.ts
+++ src/app/Pages/Projects-Page/web-app-projects/web-app-projects.component.ts
@@ -2,15 +2,31 @@ import {Component, OnInit} from '@angular/core';
 import {ISubNav} from "../../../Interfaces/sub-nav";
 import {ProjectsService} from "../../../Services/projects.service";
 import {IWebApplications} from "../../../Interfaces/web-applications";
+import {trigger, transition, style, animate} from '@angular/animations';
 
 @Component({
   selector: 'app-web-app-projects',
   templateUrl: './web-app-projects.component.html',
-  styleUrls: ['./web-app-projects.component.scss']
+  styleUrls: ['./web-app-projects.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({transform: 'translateX(100%)'}),
+        animate('.7s', style({transform: 'translateX(0%)'}))
+      ]),
+      transition(':leave', [
+        style({transform: 'translateX(0%)'}),
+        animate('.7s', style({transform: 'translateX(100%)'}))
+      ])
+    ])
+  ],
 })
 export class WebAppProjectsComponent implements OnInit {
   subNavRoutes: ISubNav[] = [];
-  webApplications: IWebApplications[] = []
+  webApplications: IWebApplications[] = [];
+  webApplicationsFiltered: IWebApplications[] = [];
+  myCategory: string[] = [];
+  showProgress: boolean = true;
 
   constructor(private projectsService: ProjectsService) {
   }
@@ -26,6 +42,22 @@ export class WebAppProjectsComponent implements OnInit {
   getWebApplications(): void {
     this.projectsService.getWebApplications().subscribe(res => {
       this.webApplications = res;
+      this.webApplicationsFiltered = this.webApplications;
+      this.myCategory = this.webApplications.map(c => c.category)
+      let myCategories = new Set(this.myCategory);
+      this.myCategory = [...myCategories];
+      this.myCategory = ['All'].concat(this.myCategory)
+      this.showProgress = false;
     })
+  }
+
+  filterByCategory(category: string): void {
+    if (category === 'All') {
+      console.log(this.webApplications);
+      this.webApplicationsFiltered = this.webApplications;
+    } else {
+      this.webApplicationsFiltered = this.webApplications.filter(c => c.category === category);
+      console.log(this.webApplications.filter(c => c.category === category));
+    }
   }
 }
