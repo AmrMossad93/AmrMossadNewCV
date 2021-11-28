1a231a0aee33003012fd332fc4d436df30dafc0b
AmrMossad93 <50120275+AmrMossad93@users.noreply.github.com>
2021-11-28 04:07:08 +0200

    Mobile Apps

--- src/app/Pages/Projects-Page/mobile-app-projects/mobile-app-project-detail/mobile-app-project-detail.component.ts
+++ src/app/Pages/Projects-Page/mobile-app-projects/mobile-app-project-detail/mobile-app-project-detail.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {IMobileApplications} from "../../../../Interfaces/mobile-applications";
+import {ProjectsService} from "../../../../Services/projects.service";
+import {OwlOptions} from "ngx-owl-carousel-o";
 
 @Component({
   selector: 'app-mobile-app-project-detail',
@@ -8,8 +11,33 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class MobileAppProjectDetailComponent implements OnInit {
   projectId: number = 0;
-
-  constructor(private activeRoute: ActivatedRoute) {
+  mobileApplication = {} as IMobileApplications;
+  showProgress: boolean = true;
+  customOptions: OwlOptions = {
+    loop: false,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
+  constructor(private activeRoute: ActivatedRoute, private projectsService: ProjectsService) {
     this.activeRoute.params.subscribe(params => {
       if (params['projectId']) {
         this.projectId = +params['projectId']
@@ -18,6 +46,13 @@ export class MobileAppProjectDetailComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.getMobileApplication()
   }
 
+  getMobileApplication(): void {
+    this.projectsService.getMobileApplications().subscribe(res => {
+      this.mobileApplication = <IMobileApplications>res.find(c => c.id === this.projectId);
+      this.showProgress = false;
+    })
+  }
 }
